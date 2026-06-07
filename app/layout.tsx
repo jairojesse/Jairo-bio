e51fac0e115dff1e00eaae321572a725c1eb69ce
jairojesse <jaiirojesse@icloud.com>
2026-06-07 19:18:40 -0300

    Atualizar o layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,400;12..96,600;12..96,700&family=Inter:wght@400;500&display=swap"
           rel="stylesheet"
         />
       </head>
@@ -29,3 +29,4 @@ export default function RootLayout({
     </html>
   );
 }
+
